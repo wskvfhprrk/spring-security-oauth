aed1d5c1b8bc0c108b26ee5cb731f92d89e3cf84
wskvfhprrk <75412985@qq.com>
2020-11-17 16:04:36 +0800

    添加resourceService项目

---
+++ resource-server/src/test/java/com/hejz/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hejz.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
